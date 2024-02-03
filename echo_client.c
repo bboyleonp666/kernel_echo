b0206a06eb589c69219cdd4b454f2ae84850c10c
bboyleonp666 <qw80818181@gmail.com>
2024-02-03 09:30:00 +0000

    Create echo server and client
    Now the echo server will experience core dump when client sending
    message to the server. Try troubleshooting ...

---
+++ echo_client.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#define DEFAULT_SERVER_IP "127.0.0.1"
+#define MAX_MSG_SIZE 4096
+
+void usage() {
+    fprintf(stdout, "Usage: %s [<server_addr>] <server_port> <message>\n", __FILE__);
+}
+
+int main(int argc, char **argv) {
+    int server_port;
+    char *server_ip, *message;
+
+    if (argc == 2 && (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0)) {
+        usage();
+        exit(EXIT_SUCCESS);
+    } else if (argc == 3) {
+        server_ip = DEFAULT_SERVER_IP;
+        server_port = atoi(argv[1]);
+        message = argv[2];
+    } else if (argc == 4) {
+        server_ip = argv[1];
+        server_port = atoi(argv[2]);
+        message = argv[3];
+    } else {
+        usage();
+        exit(EXIT_FAILURE);
+    }
+
+    ssize_t msg_len = strlen(message);
+    if (msg_len > MAX_MSG_SIZE) {
+        fprintf(stderr, "[error] message longer than limit: %d\n", MAX_MSG_SIZE);
+        exit(EXIT_FAILURE);
+    }
+
+    struct sockaddr_in server_addr = {
+        .sin_family = AF_INET,
+        .sin_port = htons(server_port),
+        .sin_addr = inet_addr(server_ip)
+    };
+
+    fprintf(stdout, "[info] create client socket\n");
+    int sock_fd;
+    if ((sock_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("[error] create client socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    fprintf(stdout, "[info] connect to %s:%d\n", server_ip, server_port);
+    int status;
+    if ((status = connect(sock_fd, (struct sockaddr *) &server_addr, 
+        sizeof(server_addr))) < 0) {
+        perror("[error] connect to server failed");
+        exit(EXIT_FAILURE);
+    }
+    send(sock_fd, message, msg_len, 0);
+    printf("[message] '%s' sent to %s:%d\n", message, DEFAULT_SERVER_IP, 
+        server_port);
+
+    close(sock_fd);
+
+    return 0;
+}
---
+++ echo_server.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#define DEFAULT_SERVER_PORT 9453
+#define MAX_MSG_SIZE 4096
+#define MAX_CONN_BACKLOG 1024
+
+void usage() {
+    fprintf(stdout, "Usage: %s [<server_port>]\n", __FILE__);
+}
+
+int main(int argc, char **argv) {
+    int server_port;
+
+    if (argc == 1) {
+        server_port = DEFAULT_SERVER_PORT;
+    } else if (argc == 2) {
+        if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0) {
+            usage();
+            exit(EXIT_SUCCESS);
+        } else if (atoi(argv[1]) > 0 & atoi(argv[1]) < 65536){
+            server_port = atoi(argv[1]);
+        } else {
+            fprintf(stderr, "[error] invalid server port: %s\n", argv[1]);
+            exit(EXIT_FAILURE);
+        }
+    } else {
+        usage();
+        exit(EXIT_FAILURE);
+    }
+
+    /* create struct for socket */
+    struct sockaddr_in server_addr = {
+        .sin_family = AF_INET,
+        .sin_port = htons(server_port),
+        .sin_addr = htonl(INADDR_ANY)
+    };
+    socklen_t sock_len = sizeof(server_addr);
+
+    /* create struct for client */
+    struct sockaddr_in client_addr;
+    socklen_t client_len = sizeof(client_addr);
+
+    /* establish a socket */
+    fprintf(stdout, "[info] create server socket\n");
+    int sock_fd;
+    if ((sock_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("[error] create server socket");
+        exit(EXIT_FAILURE);
+    }
+
+    /* bind the socket */
+    fprintf(stdout, "[info] bind server socket to port: %d\n", server_port);
+    if (bind(sock_fd, (struct sockaddr *) &server_addr, sock_len) < 0) {
+        perror("[error] bind server socket");
+        exit(EXIT_FAILURE);
+    }
+
+    /* listen to the socket with port */
+    fprintf(stdout, "[info] listen to server socket with port: %d\n", 
+        server_port);
+    if (listen(sock_fd, MAX_CONN_BACKLOG) < 0) {
+        fprintf(stderr, "[error] listen to server socket with port: %d\n", 
+            server_port);
+        exit(EXIT_FAILURE);
+    }
+
+    // while (true) {
+
+    /* accept the incoming connection attempts */
+    int client = accept(sock_fd, (struct sockaddr *) &server_addr, &sock_len);
+    if (client < 0) {
+        perror("[error] accept client");
+        exit(EXIT_FAILURE);
+    }
+
+    /* echo the message from the client */
+    char buf[MAX_MSG_SIZE] = { 0 };
+    ssize_t msg_sz = read(client, buf, MAX_MSG_SIZE);
+    printf("[message] '%s' accepted from %s:%d\n", buf, 
+        inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+    // }
+
+    /* close the socket fd */
+    close(sock_fd);
+
+    return 0;
+}
