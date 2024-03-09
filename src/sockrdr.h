7994f87a6f9399f4672c89246fc1643bd2d77f15
bboyleonp666 <qw80818181@gmail.com>
2024-03-09 09:41:21 +0000

    [MOD] make the socket map loadable
    make redirect program loadable
    replace map type: BPF_MAP_TYPE_HASH -> BPF_MAP_TYPE_SOCKHASH

--- src/sockrdr.h
+++ src/sockrdr.h
@@ -1,6 +1,15 @@
 #ifndef __SOCKRDR_H
 #define __SOCKRDR_H
 
+#define AF_INET 2
 #define MAX_MAP_ENTRIES 4096
+
+struct sock_key {
+    __u32 family;
+    __u32 remote_ip4;
+    __u32 local_ip4;
+    __u16 remote_port;
+    __u16 local_port;
+};
 
 #endif /* __SOCKRDR_H */
